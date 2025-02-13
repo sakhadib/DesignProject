e8b6deb93d2f7091a0fad222744a2af5ee72979b
sakhadib <adibsakhawat@iut-dhaka.edu>
2025-02-13 13:05:07 +0600

    add time conversion methods and improve error handling in submission process

--- backendV3/app/Http/Controllers/Controller.php
+++ backendV3/app/Http/Controllers/Controller.php
@@ -9,4 +9,31 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    /**
+     * function to convert UTC time to BDT time
+     * 
+     * @param string $UTC
+     * @return string
+     */
+    public function convertUTCToBDT($UTC)
+    {
+        $utc = new \DateTime($UTC);
+        $utc->setTimezone(new \DateTimeZone('Asia/Dhaka'));
+        return $utc->format('Y-m-d H:i:s');
+    }
+
+
+    /**
+     * function to convert BDT time to UTC time
+     * 
+     * @param string $BDT
+     * @return string
+     */
+    public function convertBDTToUTC($BDT)
+    {
+        $bdt = new \DateTime($BDT);
+        $bdt->setTimezone(new \DateTimeZone('UTC'));
+        return $bdt->format('Y-m-d H:i:s');
+    }
 }
